76a1c8e7c8d1a7b09b5bce506fd5caaa6b061d9d
wangxinbinTesla <1285173093@qq.com>
2026-06-23 12:01:21 +0800

    新增：完成本地套接字接口类的封装，和套接字功能的实现

---
+++ ServerLinux/ServerLinux/Socket.cpp
@@ -0,0 +1,277 @@
+﻿#include "Socket.h"
+
+Buffer::Buffer(size_t size) : std::string()
+{
+	resize(size);
+}
+
+Buffer::operator char* ()
+{
+	return (char*)c_str();
+}
+
+Buffer::operator char* () const
+{
+	return (char*)c_str();
+}
+
+Buffer::operator const char* () const
+{
+	return c_str();
+}
+
+CSockParam::CSockParam()
+{
+	bzero(&addr_in, sizeof(addr_in));
+	bzero(&addr_un, sizeof(addr_un));
+	port = -1;
+	attr = 0;//默认客户端、阻塞、TCP
+}
+
+CSockParam::CSockParam(const Buffer& ip, short port, int attr)
+{
+	this->ip = ip;
+	this->port = port;
+	this->attr = attr;
+	addr_in.sin_family = AF_INET;
+	addr_in.sin_port = port;
+	addr_in.sin_addr.s_addr = inet_addr(ip);
+}
+
+CSockParam::CSockParam(const Buffer& path, int arrt)
+{
+	ip = path;
+	this->attr = arrt;
+	addr_un.sun_family = AF_UNIX;
+	strcpy(addr_un.sun_path, path);
+}
+
+CSockParam::CSockParam(const CSockParam& param)
+{
+	ip = param.ip;
+	port = param.port;
+	attr = param.attr;
+	memcpy(&addr_in, &param.addr_in, sizeof(addr_in));
+	memcpy(&addr_un, &param.addr_un, sizeof(addr_un));
+}
+
+CSockParam& CSockParam::operator=(const CSockParam& param)
+{
+	if (this != &param)
+	{
+		ip = param.ip;
+		port = param.port;
+		attr = param.attr;
+		memcpy(&addr_in, &param.addr_in, sizeof(addr_in));
+		memcpy(&addr_un, &param.addr_un, sizeof(addr_un));
+	}
+
+	return *this;
+}
+
+sockaddr* CSockParam::addrin()
+{
+	return(sockaddr*)&addr_in;
+}
+
+sockaddr* CSockParam::addrun()
+{
+	return(sockaddr*)&addr_un;
+}
+
+CSocketBase::CSocketBase()
+{
+	m_socket = -1;
+	m_status = 0;
+}
+
+CSocketBase::~CSocketBase()
+{
+	Close();
+}
+
+int CSocketBase::Close()
+{
+	m_status = 3;
+	if (m_socket != -1)
+	{
+		int fd = m_socket;
+		m_socket = -1;
+		close(fd);
+	}
+	return 0;
+}
+
+CLocalSocket::CLocalSocket():CSocketBase()
+{
+}
+
+CLocalSocket::CLocalSocket(int sock) : CSocketBase()
+{
+	m_socket = sock;
+}
+
+CLocalSocket::~CLocalSocket()
+{
+	Close();
+}
+
+int CLocalSocket::Init(const CSockParam& param)
+{
+	if (m_status != 0)
+	{
+		return -1;
+	}
+	m_param = param;
+	int type = (m_param.attr & SOCK_ISUDP) ? SOCK_DGRAM : SOCK_STREAM;
+	if(m_socket == -1)
+	{
+		m_socket = socket(PF_LOCAL, type, 0);
+	}
+	if (m_socket == -1)
+	{
+		return -2;
+	}
+
+	int nRet = 0;
+	if (m_param.attr & SOCK_ISSERVER)//判断是否是服务器
+	{
+		nRet = bind(m_socket, m_param.addrun(), sizeof(sockaddr_un));
+		if (nRet == -1)
+		{
+			if (errno != EAGAIN)
+			{
+				return -3;
+			}
+		}
+
+		nRet = listen(m_socket, 32);
+		if (nRet == -1)
+		{
+			return -4;
+		}
+	}
+
+	if (m_param.attr & SOCK_ISNONBLOCK)//判断是否阻塞
+	{
+		int option = fcntl(m_socket, F_GETFL);
+		if (option == -1)
+		{
+			return -5;
+		}
+
+		option |= O_NONBLOCK;
+		nRet = fcntl(m_socket, F_SETFL, option);
+		if (nRet == -1)
+		{
+			return -6;
+		}
+	}
+
+	m_status = 1;
+	return 0;
+}
+
+int CLocalSocket::Link(CSocketBase** pClient)
+{
+	if (m_status <= 0 || m_socket == -1)
+	{
+		return -1;
+	}
+
+	int nRet = 0;
+	if (m_param.attr & SOCK_ISSERVER)//判断是否是服务器
+	{
+		if (pClient == NULL)
+		{
+			return -2;
+		}
+
+		CSockParam param;
+		socklen_t len = sizeof(sockaddr_un);
+		int nFd = accept(m_socket, param.addrun(), &len);
+		if (nFd == -1)
+		{
+			return -3;
+		}
+
+		*pClient = new CLocalSocket(nFd);
+		if (*pClient == NULL)
+		{
+			return -4;
+		}
+		nRet = (*pClient)->Init(param);
+		if (nRet != 0)
+		{
+			delete *pClient;
+			*pClient = NULL;
+			return -5;
+		}
+	}
+	else
+	{
+		nRet = connect(m_socket, m_param.addrun(), sizeof(sockaddr_un));
+		if (nRet != 0)
+		{
+			return -6;
+		}
+	}
+
+	m_socket = 2;
+	return 0;
+}
+
+int CLocalSocket::Send(const Buffer& data)
+{
+	if (m_status < 2 || m_socket == -1)
+	{
+		return-1;
+	}
+
+	ssize_t index = 0;
+	while (index < (ssize_t)data.size())
+	{
+		ssize_t len = write(m_socket, (char*)data + index, data.size() - index);
+		if (len == 0)
+		{
+			return -2;
+		}
+		if (len < 0)
+		{
+			return -3;
+		}
+		index += len;
+	}
+
+	return 0;
+}
+
+int CLocalSocket::Recv(Buffer& data)
+{
+	if (m_status < 2 || m_socket == -1)
+	{
+		return-1;
+	}
+	ssize_t len = read(m_socket, data, data.size());
+	if (len > 0)
+	{
+		data.resize(len);
+		return (int)len;//收到数据
+	}
+	if (len < 0)
+	{
+		if (errno == EINTR || errno == EAGAIN)
+		{
+			data.clear();
+			return 0;//未收到数据
+		}
+		return -2;//发生错误
+	}
+
+	return -3;//套接字被关闭
+}
+
+int CLocalSocket::Close()
+{
+	return CSocketBase::Close();
+}
---
+++ ServerLinux/ServerLinux/Socket.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string>
+#include <fcntl.h>
+
+class Buffer : public std::string
+{
+public:
+	Buffer() : std::string() {}
+	Buffer(size_t size);
+
+	operator char* ();
+
+	operator char* () const;
+
+	operator const char* () const;
+};
+
+enum SockAttr{
+	SOCK_ISSERVER = 1, //是否服务器，1表示是，0表示客户端
+	SOCK_ISNONBLOCK = 2,   //是否非阻塞，1表示是，0表示否
+	SOCK_ISUDP = 4,		//是否为UDP，1表示UDP，0表示TCP
+
+};
+
+class CSockParam
+{
+public:
+	CSockParam();
+	CSockParam(const Buffer& ip, short port, int attr);
+	CSockParam(const Buffer& path, int arrt);
+
+	~CSockParam(){}
+
+	CSockParam(const CSockParam& param);
+
+	CSockParam& operator=(const CSockParam& param);
+
+	sockaddr* addrin();
+
+	sockaddr* addrun();
+
+public:
+	//地址
+	sockaddr_in addr_in;
+	sockaddr_un addr_un;
+	//ip、端口
+	Buffer ip;
+	short port;
+	//参考SockAttr
+	int attr;
+};
+
+class CSocketBase
+{
+public:
+	CSocketBase();
+	//传递析构操作
+	virtual ~CSocketBase();
+
+	//初始化 服务器 套接字创建、bind、listen； 客户端 套接字创建
+	virtual int Init(const CSockParam& param) = 0;
+	//链接   服务器 accept； 客户端 connect   对于UDP来讲可以忽略
+	virtual int Link(CSocketBase** pClient = NULL) = 0;
+	//收、发、关闭
+	virtual int Send(const Buffer& data) = 0;
+	virtual int Recv(Buffer& data) = 0;
+	virtual int Close();
+
+protected:
+	//套接字描述符，默认-1
+	int m_socket;
+	//状态 0未初始化 1初始化完成 2连接完成 3关闭
+	int m_status;
+	CSockParam m_param;
+};
+
+class CLocalSocket : public CSocketBase
+{
+public:
+	CLocalSocket();
+	CLocalSocket(int sock);//客户端构造函数
+	//传递析构操作
+	virtual ~CLocalSocket();
+
+	//初始化 服务器 套接字创建、bind、listen； 客户端 套接字创建
+	virtual int Init(const CSockParam& param);
+	//链接   服务器 accept； 客户端 connect   对于UDP来讲可以忽略
+	virtual int Link(CSocketBase** pClient = NULL);
+	//收、发、关闭
+	virtual int Send(const Buffer& data);
+	//接收数据 大于零，表示成功；小于零，表示失败；等于零，表示未收到但无错误
+	virtual int Recv(Buffer& data);
+	virtual int Close();
+};
+
